cfdf398afd2a1318bd63e68689f7b7fa73581465
DraggonFantasy <thedraggonfantasy@gmail.com>
2022-10-23 01:41:34 +0300

    Basic qa

--- index.js
+++ index.js
@@ -12,3 +12,43 @@ const test = () => {
         json => console.log(json)
     );
 }
+
+var activeQuestion = null;
+setTimeout(() => {
+    activeQuestion = questions[0];
+    update()
+}, 1)
+
+function update() {
+    let qtype = activeQuestion.type;
+    switch (qtype) {
+        case "yesno":
+            break;
+        case "custom":
+            break;
+    }
+}
+
+function onDrag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+function onDrop(e) {
+    e.preventDefault()
+    const id = e.dataTransfer.getData("text");
+    const draggedText = document.getElementById(id).innerText;
+    const myText = e.target.innerText;
+    e.target.innerText = draggedText;
+    e.target.classList.remove("todrop")
+    document.getElementById(id).innerText = myText;
+    // alert(e.dataTransfer.getData("text"))
+    // e.dataTransfer.setData("text", e.target.innerText);
+}
+
+function allowDrop(e) {
+    e.preventDefault()
+    e.target.classList.add("todrop")
+}
+
+function allowDropEnd(e) {
+    e.target.classList.remove("todrop")
+}
--- questions.js
+++ questions.js
@@ -2,6 +2,60 @@ function q(qtype) {
 
 }
 
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function createArangeOptions() {
+    const q_arange = document.getElementById("q_arange_options");
+    const option = q_arange.querySelector(".arange-option");
+    console.log(option)
+    for(const o of [
+        "активна діяльна життя (повнота та емоційна насиченість життя);",
+        "життєва мудрість (зрілість суджень і здоровий глузд, що досягаються життєвим досвідом);",
+        "здоров'я (фізичне і психічне);",
+        "цікава робота;",
+        "краса природи і мистецтва (переживання прекрасного в природі і в мистецтві);",
+        "любов (духовна і фізична близькість з коханою людиною);",
+        "матеріально забезпечене життя (відсутність матеріальних труднощів);",
+        "наявність хороших і вірних друзів;",
+        "суспільне покликання (повага оточуючих, колективу, товаришів по роботі);",
+        "пізнання (можливість розширення своєї освіти, кругозору, загальної культури, інтелектуальне розвиток);",
+        "продуктивна життя (максимально повне використання своїх можливостей, сил і здібностей);",
+        "розвиток (робота над собою, постійне фізичне і духовне вдосконалення);",
+        "розваги (приємне, необтяжливе проведення часу, відсутність обов'язків)",
+        "свобода (самостійність, незалежність у судженнях і вчинках)",
+        "щасливе сімейне життя",
+        "щастя інших (добробут, розвиток і вдосконалення інших людей, всього народу, людства в цілому)",
+        "творчість (можливість творчої діяльності)",
+        "впевненість у собі (внутрішня гармонія, свобода від внутрішніх протиріч; сумнівів)."
+    ]) {
+        const elem = option.cloneNode(true)
+        elem.id = makeid(5);
+        elem.innerText = o;
+        elem.style.display = "block";
+        q_arange.appendChild(elem);
+        const i = $(`#${elem.id}`)
+        i.draggable({revert: true, revertDuration: 100, axis: 'y', snap: 'inner'});
+        i.droppable({drop: function (event, ui)
+            {
+                var one = $(this).text();
+                var two = ui.draggable.text();
+                $(this).text(two);
+                ui.draggable.text(one);
+            }})
+    }
+
+}
+
 const questions = [
     q("", "", )
 ]
+
+setTimeout(createArangeOptions, 2)
